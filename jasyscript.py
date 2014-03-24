590c2d6ddf97aaa402adfa79416d161449da54ec
Sebastian Werner <info@sebastian-werner.net>
2014-03-24 12:44:09 +0100

    Indented

--- jasyscript.py
+++ jasyscript.py
@@ -1,18 +1,18 @@
 @task
 def build(regenerate=False):
-	"""Generate build version"""
+    """Generate build version"""
 
-	profile = Profile(session)
-	profile.registerPart("swerner", styleName="swerner.Main")
-	profile.setHashAssets(True)
-	profile.setCopyAssets(True)
+    profile = Profile(session)
+    profile.registerPart("swerner", styleName="swerner.Main")
+    profile.setHashAssets(True)
+    profile.setCopyAssets(True)
 
-	konstrukteur.build(profile, regenerate)
+    konstrukteur.build(profile, regenerate)
 
-	Build.run(profile)
+    Build.run(profile)
 
 
 @task
 def sync():
-  Console.info("Syncing build to public server...")
-  executeCommand("rsync --recursive --verbose --links --perms --times --compress --delete --human-readable build/asset build/css build/en build/de homepage:~/html/", wrapOutput=False)
+    Console.info("Syncing build to public server...")
+    executeCommand("rsync --recursive --verbose --links --perms --times --compress --delete --human-readable build/asset build/css build/en build/de homepage:~/html/", wrapOutput=False)
