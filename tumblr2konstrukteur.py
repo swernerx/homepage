68c355ff677404c36bb639acaf9a16dccc45d564
Sebastian Werner <info@sebastian-werner.net>
2014-03-25 09:48:55 +0100

    Worked on tumblr import

--- tumblr2konstrukteur.py
+++ tumblr2konstrukteur.py
@@ -3,6 +3,8 @@
 import requests
 import xml.etree.ElementTree as ElementTree
 import markdownify
+import hashlib
+import os.path
 
 template = """
 title: %s
@@ -12,16 +14,23 @@ date: %s
 %s
 """
 
+photoAssetFolder = "tumblr"
+photoFolder = "source/asset/%s" % photoAssetFolder
+projectName = "swerner"
+
+if not os.path.isdir(photoFolder):
+    os.mkdir(photoFolder)
+
 
 def process(url):
     pos = 0
-    fetch = 10
+    fetch = 50
     end = fetch
 
     while pos < end:
         print("Requesting %s-%s of %s" % (pos, pos+fetch, end))
 
-        response = requests.get(url % pos)
+        response = requests.get(url % (pos, fetch))
 
         if response.status_code != 200:
             raise Exception("Error during communication with Tumblr: %s" % r.status)
@@ -40,26 +49,49 @@ def process(url):
             postType = post.get("type")
             postDate = post.get("date-gmt")
             postSlug = post.get("slug")
+            postFormat = post.get("format")
 
             if postType == "quote":
-                postTitle = None
                 quoteText = post.find("quote-text").text
                 quoteSourceComment = post.find("quote-source").text
+
+                quoteText = markdownify.markdownify(quoteText).rstrip("\n")
+                quoteSourceComment = markdownify.markdownify(quoteSourceComment).rstrip("\n")
                 print(">>> QUOTE: %s " % quoteText)
 
             elif postType == "photo":
-                photoDescription = post.find("photo-caption").text
+                photoText = post.find("photo-caption").text
                 photoLinkUrl = post.find("photo-link-url").text
                 photoUrl = post.find("photo-url").text
 
-                photoText = markdownify.markdownify(photoDescription).rstrip("\n")
+                photoText = markdownify.markdownify(photoText).rstrip("\n")
                 print(">>> PHOTO: %s" % photoText)
+
+                photoExtension = os.path.splitext(photoUrl)[1]
+                photoResponse = requests.get(photoUrl)
+                photoHash = hashlib.sha1(photoResponse.content).hexdigest()
+
+                photoFileName = "%s%s" % (photoHash, photoExtension)
+                photoFile = open(os.path.join(photoFolder, photoFileName), "wb")
+                photoFile.write(photoResponse.content)
+                photoFile.close()
+
+                photoAsset = '<img src="{{@asset.url %s/%s/%s}}"/>' % (projectName, photoAssetFolder, photoFileName)
+                print(">>> PHOTO ASSET: %s" % photoAsset)
 
             elif postType == "link":
                 linkText = post.find("link-text").text
                 linkUrl = post.find("link-url").text
 
+                linkText = markdownify.markdownify(linkText).rstrip("\n")
                 print(">>> LINK: %s" % linkText)
+
+            elif postType == "regular":
+                postTitle = post.find("regular-title").text
+                postText = post.find("regular-body").text
+
+                postText = markdownify.markdownify(postText).rstrip("\n")
+                print(">>> REGULAR: %s" % postText)
 
             else:
                 print("Unknown POST-TYPE: %s" % postType)
@@ -79,4 +111,4 @@ def process(url):
 
 
 
-process("http://sebastian-werner.com/api/read?start=%s&num=10")
+process("http://sebastian-werner.com/api/read?start=%s&num=%s")
