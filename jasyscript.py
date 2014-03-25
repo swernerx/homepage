4815f93b4b5fda61caac4aa2e4aa9fe7c4a9d169
Sebastian Werner <info@sebastian-werner.net>
2014-03-25 15:16:42 +0100

    Added clean and distclean tasks

--- jasyscript.py
+++ jasyscript.py
@@ -13,6 +13,16 @@ def build(regenerate=False):
 
 
 @task
+def clean():
+    core.clean()
+
+
+@task
+def distclean():
+    core.distclean()
+
+
+@task
 def sync():
     Console.info("Syncing build to public server...")
     executeCommand("rsync --recursive --verbose --links --perms --times --compress --delete --human-readable build/asset build/css build/en build/de homepage:~/html/", wrapOutput=False)
